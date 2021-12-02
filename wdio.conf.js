9fb4d57295a1eccb3c27b135a4f99cec8c300924
prasetyo531 <prasetyo531@gmail.com>
2021-12-02 19:42:45 +0700

    make specs as 1 line

--- wdio.conf.js
+++ wdio.conf.js
@@ -20,9 +20,7 @@ exports.config = {
     // then the current working directory is where your `package.json` resides, so `wdio`
     // will be called from there.
     //
-    specs: [
-        './test/specs/**/*.js'
-    ],
+    specs: ['./test/specs/**/*.js'],
     // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
